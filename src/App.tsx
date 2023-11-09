ff45fcdc87ac7e0f605dfc446007f9dd45d84181
Pavel Kuzyakin <p.kuzyakin@eapteka.ru>
2023-11-09 15:31:40 +0400

    Новая форма

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.tsx';
 import Record from './pages/Record.tsx';
 import Navigation from './components/layout/Navigation.tsx';
+import Signup from './pages/Signup.tsx';
 
 const App = () => {
   return (
     <>
       <Navigation />
       <Routes>
+        <Route path="/signup" element={<Signup />} />
         <Route path="/records/add" element={<Record />} />
         <Route path="/records/:id" element={<Record />} />
         <Route path="/" element={<Home />} />
